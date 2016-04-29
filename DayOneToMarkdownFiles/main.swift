306e9ea9725537861b26896ebe46bb0cf74264e6
Martin Hartl <martin@hartl.co>
2016-04-29 21:01:34 +0200

    Initial Commit

---
+++ DayOneToMarkdownFiles/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DayOneToMarkdownFiles
+//
+//  Created by mhaddl on 29/04/16.
+//  Copyright © 2016 Martin Hartl. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
